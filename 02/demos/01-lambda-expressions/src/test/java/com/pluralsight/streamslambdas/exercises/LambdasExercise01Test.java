34ebb37ee7f375b081fe41880e1623dcf902c59b
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-08-11 17:33:00 +0400

    simple stream use example

--- 02/demos/01-lambda-expressions/src/test/java/com/pluralsight/streamslambdas/exercises/LambdasExercise01Test.java
+++ 02/demos/01-lambda-expressions/src/test/java/com/pluralsight/streamslambdas/exercises/LambdasExercise01Test.java
@@ -1,49 +1,45 @@
-/*
- * This code is part of the course "Working with Streams and Lambda Expressions in Java (Java SE 11 Developer Certification 1Z0-819)" for Pluralsight.
- *
- * Copyright (C) 2021 by Jesper de Jong (jesper@jdj-it.com).
- */
-package com.pluralsight.streamslambdas.exercises;
-
-import com.pluralsight.streamslambdas.Product;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static com.pluralsight.streamslambdas.Category.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
-class LambdasExercise01Test {
-
-    private static final List<Product> TEST_PRODUCTS = Arrays.asList(
-            new Product(OFFICE, "Pencils", 5.79),
-            new Product(FOOD, "Apples", 1.29),
-            new Product(UTENSILS, "Plates", 12.95),
-            new Product(FOOD, "Spaghetti", 2.79),
-            new Product(FOOD, "Coffee", 7.49),
-            new Product(CLEANING, "Detergent", 3.79));
-
-    private LambdasExercise01 exercise = new LambdasExercise01();
-
-    @Test
-    @DisplayName("Exercise 1: Sort products by name")
-    void sortProductsByName() {
-        List<Product> products = new ArrayList<>(TEST_PRODUCTS);
-
-        exercise.sortProductsByName(products);
-
-        assertThat(products)
-                .describedAs("Exercise 1: Your solution does not return the correct result.")
-                .containsExactly(
-                        new Product(FOOD, "Apples", 1.29),
-                        new Product(FOOD, "Coffee",7.49),
-                        new Product(CLEANING, "Detergent", 3.79),
-                        new Product(OFFICE, "Pencils",5.79),
-                        new Product(UTENSILS, "Plates", 12.95),
-                        new Product(FOOD, "Spaghetti", 2.79));
-    }
-}
+//
+//package com.pluralsight.streamslambdas.exercises;
+//
+//import com.pluralsight.streamslambdas.Product;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import java.math.BigDecimal;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static com.pluralsight.streamslambdas.Category.*;
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//class LambdasExercise01Test {
+//
+//    private static final List<Product> TEST_PRODUCTS = Arrays.asList(
+//            new Product(OFFICE, "Pencils", 5.79),
+//            new Product(FOOD, "Apples", 1.29),
+//            new Product(UTENSILS, "Plates", 12.95),
+//            new Product(FOOD, "Spaghetti", 2.79),
+//            new Product(FOOD, "Coffee", 7.49),
+//            new Product(CLEANING, "Detergent", 3.79));
+//
+//    private LambdasExercise01 exercise = new LambdasExercise01();
+//
+//    @Test
+//    @DisplayName("Exercise 1: Sort products by name")
+//    void sortProductsByName() {
+//        List<Product> products = new ArrayList<>(TEST_PRODUCTS);
+//
+//        exercise.sortProductsByName(products);
+//
+//        assertThat(products)
+//                .describedAs("Exercise 1: Your solution does not return the correct result.")
+//                .containsExactly(
+//                        new Product(FOOD, "Apples", 1.29),
+//                        new Product(FOOD, "Coffee",7.49),
+//                        new Product(CLEANING, "Detergent", 3.79),
+//                        new Product(OFFICE, "Pencils",5.79),
+//                        new Product(UTENSILS, "Plates", 12.95),
+//                        new Product(FOOD, "Spaghetti", 2.79));
+//    }
+//}
