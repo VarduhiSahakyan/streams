e21ad7c9e23c6341d6c2c93be6d8bb2b9149d24b
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-08-11 16:11:03 +0400

    Predicate interface use example

--- 02/demos/01-lambda-expressions/src/main/java/com/pluralsight/streamslambdas/functionalinterfacesexamples/FunctionalInterfacesExample04.java
+++ 02/demos/01-lambda-expressions/src/main/java/com/pluralsight/streamslambdas/functionalinterfacesexamples/FunctionalInterfacesExample04.java
@@ -9,30 +9,28 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 public class FunctionalInterfacesExample04 {
-    static Optional<Product> findProduct(List<Product> products , Predicate<Product> predicate){
+    static Optional<Product> findProduct(List<Product> products, Predicate<Product> predicate) {
         for (Product p : products) {
-            if (predicate.test(p)){
+            if (predicate.test(p)) {
                 return Optional.of(p);
             }
-        } return Optional.empty();
+        }
+        return Optional.empty();
     }
 
     public static void main(String[] args) {
         List<Product> products = ExampleData.getProducts();
 
         Double priceLimit = 2.00;
-
+        // Two simple predicates that can be combined using the functional composition methods in interface Predicate.
         // this predicate checks the product is food opr not
         Predicate<Product> isFood = product -> product.getCategory() == Category.FOOD;
         // this predicate checks is the prise of product less then the limit price
-        Predicate<Product> isCheap = product -> product.getPrice().compareTo(priceLimit) <0;
-
-        String name = "Spaghetti";
-
+        Predicate<Product> isCheap = product -> product.getPrice().compareTo(priceLimit) < 0;
 
         findProduct(products, isFood.and(isCheap))
                 .ifPresentOrElse(
-                        price -> System.out.printf("The price of %s is $ %.2f%n", name , price),
+                        product -> System.out.printf("The price of %s is $ %.2f%n", product.getName(), product.getPrice()),
                         () -> System.out.println("There is no cheap food product")
                 );
     }
